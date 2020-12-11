c4c857bd6a635e7b6474c9fdc2b4b7f47ae89f46
CarsonMcNeff <cjmcneff@outlook.com>
2020-12-11 12:33:11 -0600

    commit tree 2d8cb52f5c2144dc43a5764de38d3c4017fa81b7

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash.empty?
+        return nil
+    end
+    smallkey = name_hash.first.first
+    name_hash.each do |key, value|
+        if value < name_hash[smallkey]
+            smallkey = key
+        end
+    end
+    smallkey
 end
